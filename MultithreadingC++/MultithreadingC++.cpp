8a636bc0b4400ca4e2629b0e5472495c73d38a92
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-22 21:14:30 +0330

    launch the process on multiple threads

--- MultithreadingC++/MultithreadingC++.cpp
+++ MultithreadingC++/MultithreadingC++.cpp
@@ -4,15 +4,27 @@
 #include <random>
 #include <cmath>
 #include <limits>
+#include <thread>
 #include "NastihanTimer.h"
 
 
 constexpr size_t DATASET_SIZE = 5000000;
 
+void Process(std::array<int, DATASET_SIZE>& set)
+{
+    for (auto x : set)
+    {
+        constexpr auto limit = (double)std::numeric_limits<int>::max();
+        const auto y = (double)x / limit;
+        set[0] += int(std::sin(std::cos(y)) * limit);
+    }
+}
+
 int main()
 {
     std::minstd_rand rne;
     std::vector<std::array<int, DATASET_SIZE>> datasets{ 4 };
+    std::vector<std::thread> workers;
 
     for (auto& set : datasets)
     {
@@ -22,12 +34,11 @@ int main()
     NastihanTimer timer;
     for (auto& set : datasets)
     {
-        for (auto x : set)
-        {
-            constexpr auto limit = (double)std::numeric_limits<int>::max();
-            const auto y = (double)x / limit;
-            set[0] += int(std::sin(std::cos(y)) * limit);
-        }
+        workers.push_back(std::thread(Process, std::ref(set)));
+    }
+    for (auto& w : workers)
+    {
+        w.join();
     }
     auto t = timer.Peek();
 
