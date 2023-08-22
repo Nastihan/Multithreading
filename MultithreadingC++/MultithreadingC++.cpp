3b5cbb3d1171d747a3de6dbc0fe647f85abcffc7
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-22 20:40:19 +0330

    do processing on the datasets and output its time

--- MultithreadingC++/MultithreadingC++.cpp
+++ MultithreadingC++/MultithreadingC++.cpp
@@ -14,16 +14,26 @@ int main()
     std::minstd_rand rne;
     std::vector<std::array<int, DATASET_SIZE>> datasets{ 4 };
 
-    NastihanTimer timer;
     for (auto& set : datasets)
     {
         std::ranges::generate(set, rne);
+    }
+
+    NastihanTimer timer;
+    for (auto& set : datasets)
+    {
+        for (auto x : set)
+        {
+            constexpr auto limit = (double)std::numeric_limits<int>::max();
+            const auto y = (double)x / limit;
+            set[0] += int(std::sin(std::cos(y)) * limit);
+        }
     }
     auto t = timer.Peek();
 
 
 
-    std::cout << "Generation took: " << t << " seconds\n";
+    std::cout << "Processing took: " << t << " seconds\n";
     return 0;
 }
 
