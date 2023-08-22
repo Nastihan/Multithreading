c16ccd4102eecc3ad5b1d80c7105f7966775822d
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-22 20:10:19 +0330

    generate a random big dataset

--- MultithreadingC++/MultithreadingC++.cpp
+++ MultithreadingC++/MultithreadingC++.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <array>
+#include <random>
+
+constexpr size_t DATASET_SIZE = 5000000;
 
 int main()
 {
+    std::minstd_rand rne;
+    std::vector<std::array<int, DATASET_SIZE>> datasets{ 4 };
+
+    for (auto& arr : datasets)
+    {
+        std::ranges::generate(arr, rne);
+    }
+
     std::cout << "Hello World!\n";
     return 0;
 }
